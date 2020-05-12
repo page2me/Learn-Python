2ba299113064739a9437225df23bded6a964f65a
page2me kitarotao <page2me@msn.com>
2020-05-12 22:57:34 +0700

    Update comments.py

--- comments.py
+++ comments.py
@@ -1,5 +1,5 @@
-#This is a single line comment in Python
-print("Hello World") #This is a single comment in Python
+# This is a single line comment in Python
+print("Hello World") # This is a single comment in Python
 """ For multi-line
 comment use three
 double quotes
