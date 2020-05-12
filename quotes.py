0938009eb1c4451f4c13e19bde621987d7037694
page2me kitarotao <page2me@msn.com>
2020-05-12 23:01:34 +0700

    Create quotes.py

---
+++ quotes.py
@@ -0,0 +1,15 @@
+# Triple, double and single quotes
+print("Hello World!")
+print('Hello World!')
+
+# single quotes three times
+print('''I am mad in love
+do you think
+I am doing
+the right thing ''')
+
+# double quotes three times
+print("""I am mad in love
+do you think
+I am doing
+the right thing """)
